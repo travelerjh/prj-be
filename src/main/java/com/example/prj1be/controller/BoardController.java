ff68c380b6af5ee0f022af9929c9dfcb840aed2d
Jae Hee <travel237corp@naver.com>
2023-11-09 11:45:57 +0900

    최초 설정 및 boardcontroller /api/borad/add 작성

---
+++ src/main/java/com/example/prj1be/controller/BoardController.java
@@ -0,0 +1,20 @@
+package com.example.prj1be.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/board")
+
+public class BoardController {
+
+    @PostMapping("add")
+    public void add(){
+        System.out.println("boardcontroller.add");
+    }
+
+}
