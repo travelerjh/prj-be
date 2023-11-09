ab322a7e0cee737bc5fb5f1148bfcfe7589e0128
Jae Hee <travel237corp@naver.com>
2023-11-09 14:17:16 +0900

    null 이거나 isBlank 인경우 badrequest 응답

--- src/main/java/com/example/prj1be/controller/BoardController.java
+++ src/main/java/com/example/prj1be/controller/BoardController.java
@@ -21,6 +21,12 @@ public class BoardController {
     //json으로 받는게 requestBody
     @PostMapping("add")
     public ResponseEntity add(@RequestBody Board board) {
+
+        if(!service.validate(board)){
+            return ResponseEntity.badRequest().build();
+        }
+
+
         if (service.save(board)) {
             return ResponseEntity.ok().build();
         } else {
--- src/main/java/com/example/prj1be/service/BoardService.java
+++ src/main/java/com/example/prj1be/service/BoardService.java
@@ -15,4 +15,20 @@ public class BoardService {
     public boolean save(Board board) {
       return mapper.insert(board) ==1;
     }
+
+
+    // 검증하는거
+    public boolean validate(Board board) {
+        if(board ==null){
+            return false;
+        }if (board.getContent()==null||board.getContent().isBlank()){
+            return false;
+        }if (board.getTitle()==null||board.getTitle().isBlank()){
+            return  false;
+        }if (board.getWriter()==null ||board.getWriter().isBlank()){
+            return false;
+        }
+        return true;
+
+    }
 }
